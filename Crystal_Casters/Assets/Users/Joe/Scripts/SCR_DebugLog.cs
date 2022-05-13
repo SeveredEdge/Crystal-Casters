2fb4fc1e355404379b922167ad32d07f1b125305
Joe Barlow <joebarlow0800@gmail.com>
2022-05-13 16:17:48 +0100

    Crystal Teleportation Implemented

---
+++ Crystal_Casters/Assets/Users/Joe/Scripts/SCR_DebugLog.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SCR_DebugLog : MonoBehaviour
+{
+    public static SCR_DebugLog Instance { get; private set; }
+
+    private TextMeshProUGUI log;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void Print(string str)
+    {
+        if (log == null)
+        {
+            log = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        }
+
+        log.text += "\n" + str;
+    }
+}
